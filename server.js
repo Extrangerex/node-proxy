c14465bf6f314918fa606c7e0886f73c83fb789c
Elliot Pichardo <jopichardomartinez@gmail.com>
2024-09-01 16:57:01 -0400

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const request = require('request');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/image/:fileId', (req, res) => {
   const fileId = req.params.fileId;
