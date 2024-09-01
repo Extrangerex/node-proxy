c7079b4e7961a3d738980bbdd43ed18c177e1e19
Elliot Pichardo <jopichardomartinez@gmail.com>
2024-09-01 16:44:55 -0400

    node proxy

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/image/:fileId', (req, res) => {
+  const fileId = req.params.fileId;
+  const driveUrl = `https://drive.google.com/uc?export=view&id=${fileId}`;
+
+  request.get(driveUrl).pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
